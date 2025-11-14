f6b276c0da866fa008f5034b2f41cb729079a2b5
Anay Choubey <anaychoubey@suttongrammar.school>
2025-11-14 22:57:59 +0000

    Adding all files after around 1hr of working on them!
    Add files (including photos).

---
+++ space.js
@@ -0,0 +1,63 @@
+//Declaring variables
+let tileSize = 32;
+let rows = 16;
+let columns = 16;
+
+let board;
+let boardWidth = tileSize * columns;
+let boardHeight = tileSize * rows;
+let context;
+
+let shipWidth = tileSize*2;
+let shipHeight = tileSize;
+let shipX = tileSize * columns/2 - tileSize;
+let shipY = tileSize * rows - tileSize*2;
+
+let ship = {
+    x : shipX,
+    y : shipY,
+    width : shipWidth,
+    height : shipHeight,
+}
+
+let shipImg;
+let shipVelocityX = tileSize;
+
+
+
+window.onload = function() {
+  board = document.getElementById("board");
+  board.width=boardWidth;
+  board.height= boardHeight;
+  context = board.getContext("2d");
+  //green rectangle:
+  //context.fillStyle = "green";
+  //context.fillRect(ship.x, ship.y, ship.width, ship.height);
+
+  // loading images:
+  shipImg = new Image();
+  shipImg.src = "./ship.png";
+  shipImg.onload = function() {
+    context.drawImage(shipImg, ship.x, ship.y, ship.width, ship.height);
+  }
+  requestAnimationFrame(update);
+  this.document.addEventListener("keydown",moveShip);
+}
+
+
+function update() {
+  requestAnimationFrame(update);
+  context.clearRect(0, 0, board.width, board.height);
+  context.drawImage(shipImg, ship.x, ship.y, ship.width, ship.height);
+
+}
+
+function moveShip(e) {
+    if(e.code == "ArrowLeft"){
+        ship.x-=shipVelocityX;
+    }
+    else if (e.code == "ArrowRight"){
+        ship.x += shipVelocityX;
+    }
+
+}
